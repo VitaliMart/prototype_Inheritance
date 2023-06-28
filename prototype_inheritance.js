80d200a4897f49b2e427905419fc67f242a5a4b6
Vitali Martsinchyk <vitali.martinchik@gmail.com>
2023-06-28 17:08:46 +0300

    feat: add decide task

---
+++ prototype_inheritance.js
@@ -0,0 +1,20 @@
+let animal = {
+    eats: true,
+    move() {
+        return 'I am walking';
+    }
+};
+let cat = {
+    say: 'meow',
+    greetigs(move) {
+        if (move) {
+            console.log(this.move() + " " + this.say + " owner");
+        } else {
+            console.log(this.say);
+        }
+    },
+    __proto__: animal
+};
+
+cat.greetigs(true);
+cat.greetigs(false);
